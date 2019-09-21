1160c22f43e32541035601219141e500da05be51
LalitKishor <lalitsinghbanna05@gmail.com>
2019-09-21 11:33:15 +0530

    change env

--- udacity-c3-frontend/src/environments/environment.prod.ts
+++ udacity-c3-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a3b2ff2b4dc2e11e98d6f16097f0958b-1666325597.us-east-1.elb.amazonaws.com/:8080/api/v0'
+  apiHost: 'http://a43d18343dc3511e98d6f16097f0958b-1642057495.us-east-1.elb.amazonaws.com/:8080/api/v0'
 };
 
 /*
